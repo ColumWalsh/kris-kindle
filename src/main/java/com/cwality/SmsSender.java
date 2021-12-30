c2de33d7f997e32941becfa18ee738e63e6791df
Colum <colum@columwalsh.ie>
2021-12-30 21:48:00 +0000

    catch and log message sending exceptions, change message

--- src/main/java/com/cwality/SmsSender.java
+++ src/main/java/com/cwality/SmsSender.java
@@ -24,9 +24,12 @@ public class SmsSender {
     @Value("${twilio.number}")
     private String twilioNumber;
 
-    private String BODY_TEMPLATE = "Hi %s, you're in a Kris Kindle."
-            + " There's a €%s price limit and you're getting a present for %s.";
+    @Value("${twilio.us.number}")
+    private String twilioUsNumber;
 
+    private static String BODY_TEMPLATE = "Hi %s, you're %s's secret santa."
+            + " Get them something nice! The limit is €%s.";
+            
     private static final Logger logger = LoggerFactory.getLogger(SmsSender.class);
 
     protected List<Match<Person>> sendMessages(List<Person> participants, boolean dryRun, Integer limit) {
@@ -35,18 +38,29 @@ public class SmsSender {
         GraphJumbler.validateKindleMatches(matches);
         for (Match<Person> match : matches) {
             
-            String body = String.format(BODY_TEMPLATE, match.giver.name, limit, match.taker.name);
+            String body = String.format(BODY_TEMPLATE, match.giver.name, match.taker.name, limit);
             String from = twilioNumber;
+            if(match.giver.number.startsWith("+1")) {
+                logger.info("getting us number");
+                from = twilioUsNumber;
+            }
             if (dryRun == true) {
                 logger.info("dry run for gifter {} to giftee {}", match.giver.name, match.taker.name);
                 logger.info("from: {}", from);
                 logger.info("to: {}", match.giver.number);
+                if(match.giver.number.startsWith("+1")) {
+                    logger.info("god damn canadians");
+                }
                 logger.info("body: {}", body);
             } else {
-                Message message = Message.creator(new PhoneNumber(match.giver.number), // to
-                        new PhoneNumber(from), // from
-                        body).create();
-                logger.info("message sid is {}", message.getSid());
+                try {
+                    Message message = Message.creator(new PhoneNumber(match.giver.number), // to
+                            new PhoneNumber(from), // from
+                            body).create();
+                    logger.info("message sid is {}", message.getSid()); 
+                } catch (Exception e) {
+                    logger.error("failed sending message to number: {} failed. message: {}.", match.giver.number, body, e);
+                }
             }
         }
         return matches;
